0326c5bdd36cc08c28a3f819dfa2956412bed084
ggrl <gabrielgoes@usp.br>
2024-03-17 22:21:26 -0300

    comit para o Deru

--- back4.sh
+++ back4.sh
@@ -1,14 +1,14 @@
 #!/bin/env bash
 select1='feh --bg-fill --no-fehbg '
-select2='xwallpaper --zoom '
+select2='xwallpaper --focus '
 select3='xloadimage -onroot -fullscreen '
 select4='xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s '
 select5='hsetroot -full '
 select6='gsettings set org.gnome.desktop.background picture-uri file:///'
 
-prog=$select1
+prog=$select2
 [ "$#" -lt "1" ] || [ "$#" -gt "2" ] && {  echo -e "ERROR : args number invalid \n $0 speed /path/name.gif" ; echo "try 0.010 as speed" ; exit 1 ; }
-dir=/tmp/back4  
+dir=/tmp/back4
 #example of speed  : 0.010
 speed=$1
 name=$2
@@ -19,7 +19,7 @@ name=$2
 [  -f "/usr/local/share/gif-wallpaper/loading.png" ] && ${select1}/usr/local/share/gif-wallpaper/loading.png
 hash=`md5sum $name | cut -f1 -d" "`
 
-[[ ! -d $dir ]] && mkdir $dir 
+[[ ! -d $dir ]] && mkdir $dir
 
 [[ ! -d $dir/$hash ]] && { mkdir $dir/$hash ; echo "spliting .." ; convert -coalesce $name $dir/$hash/$hash.png ; echo ok ; }
 
@@ -30,5 +30,4 @@ if [ "$1" == "auto" ]; then
     speed=$(bc <<< "scale=3; ($amount_of_frames/$frames_per_second) * (1/$frames_per_second)")
 fi
 
-while : ; do for i in ` ls $dir/$hash -v ` ; do $prog$dir/$hash/$i ; sleep $speed ; done ; done    
-   
+while : ; do for i in ` ls $dir/$hash -v ` ; do $prog$dir/$hash/$i ; sleep $speed ; done ; done
